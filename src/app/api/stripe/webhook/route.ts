53ce58744cc75fef1455594e13a4a676dc2e526e
mohamed11933 <mohamedhamed11933@gmail.com>
2025-09-17 21:08:39 +0200

    fixing stripe api dynamic version

--- src/app/api/stripe/webhook/route.ts
+++ src/app/api/stripe/webhook/route.ts
@@ -3,9 +3,7 @@ import Stripe from 'stripe'
 import { getAdminSupabase } from "@/lib/usage"
 const supabase = getAdminSupabase()
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-06-20',
-})
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
 
 export async function POST(req: Request) {
   const body = await req.text()
